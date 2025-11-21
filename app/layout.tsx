16717f5e743be94c0781254e7f8f221dc8113ea4
daniel-bss <daniel.simamora2000@gmail.com>
2025-11-21 20:44:11 +0700

    fix: main layout min-h

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.className} bg-primary`}>
-      <body className="min-h-[100dvh] body-main-background">
+      <body className="body-main-background">
         <SWRConfig
           value={{
             fallback: {
