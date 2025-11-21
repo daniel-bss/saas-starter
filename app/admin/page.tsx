05fa8a5a338c432e2fd7498988e249e04489df81
daniel-bss <daniel.simamora2000@gmail.com>
2025-11-22 06:00:14 +0700

    checkpoint: 11/22/2025

---
+++ app/admin/page.tsx
@@ -0,0 +1,10 @@
+import { cookies } from "next/headers";
+
+export default function Page() {
+  //   const token = cookies().get("token")?.value;
+
+  // decode or fetch user info from backend
+  //   const user = await getUserFromToken(token);
+
+  return <div>ADMIN</div>;
+}
