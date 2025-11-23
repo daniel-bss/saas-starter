8dda0daf348a7e17b89637278a0dbcbc71ac1e12
daniel-bss <daniel.simamora2000@gmail.com>
2025-11-23 19:20:45 +0700

    checkpoint Sun 23 Nov 19.20

--- app/login/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Pagex() {
-  return <div className="bg-green-50 mt-[300px]">hehehehe</div>;
-}
---
+++ app/no-page.tsx
@@ -0,0 +1,18 @@
+/**
+NOTE:
+Root Page is in the "/app/(home)/page.tsx"
+
+
+TODO:
+rename (loginz), set sign-in endpoint to login instead
+
+define schema (zod) in "/lib/auth/actions.ts"
+import in "/app/login/actions.ts"
+
+
+TODO: JWT or session
+
+TODO: gtag (Google Manager)
+
+TODO: /lib/api/ to define "fetcher.ts"
+*/
